d8bd283e3648299c55b679aa34263f585234a0f6
Devendra Prasad <devendraprasad1984@gmail.com>
2021-08-11 20:05:54 +0530

    update

--- first-test/src/App.svelte
+++ first-test/src/App.svelte
@@ -1,10 +1,20 @@
 <script>
     import NameAge from './components/nameAge.svelte'
+
     export let appHeader
+    let people = [
+        {name: 'devendra', age: 38},
+        {name: 'jyoti', age: 24},
+        {name: 'kumar', age: 34},
+        {name: 'latte', age: 12},
+        {name: 'noob', age: 21},
+        {name: 'deva ki adalat', age: 23},
+    ]
 </script>
 
 <div class="container">
     <h1 class="text-primary">{appHeader}</h1>
-    <NameAge name="Devendra Prasad" age={38}/>
-    <NameAge name="Jyoti Sharma" age={35}/>
+    {#each people as p}
+        <NameAge {...p}/>
+    {/each}
 </div>
