4a42e6dcbcf2fe84c874d367eb3e55102ff5b543
Devendra Prasad <devendraprasad1984@gmail.com>
2021-08-11 15:36:41 +0530

    update

--- first-test/src/App.svelte
+++ first-test/src/App.svelte
@@ -1,5 +1,9 @@
 <script>
     export let name, age;
+    let toggleControls = false
+    const handleControlsToggle = () => {
+        toggleControls = !toggleControls
+    }
     const changeAge = (num) => {
         age = age + num
     }
@@ -8,12 +12,22 @@
     h1 {
         color: var(--primary-color);
     }
+
+    h3 {
+        margin-bottom: 20px;
+    }
 </style>
 <div class="container">
     <div class="card">
         <h1>Hello {name}!</h1>
         <h3>i am {age} yrs old</h3>
-        <button class="btn btn-primary" on:click={()=>changeAge(1)}>+1</button>
-        <button class="btn btn-danger" on:click={()=>changeAge(-1)}>-1</button>
+        <h3><button class="btn btn-primary" on:click={handleControlsToggle}>Toggle Controls</button></h3>
+
+        {#if toggleControls === true}
+            <button class="btn btn-primary" on:click={()=>changeAge(1)}>+1</button>
+            <button class="btn btn-danger" on:click={()=>changeAge(-1)}>-1</button>
+            <div>binding age to input</div>
+            <input type="number" bind:value={age}/>
+        {/if}
     </div>
 </div>
